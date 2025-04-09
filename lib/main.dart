32b83f77c15d8fa52ff9a72821fe30a5b6491f7c
martabella <mbella@lasalle.cat>
2025-04-09 19:22:47 +0200

    widgets

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,57 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: ContentView(),
       ),
     );
+  }
+}
+
+class ContentView extends StatefulWidget {
+  const ContentView({super.key});
+
+  @override
+  State<ContentView> createState() => _ContentViewState();
+}
+
+class _ContentViewState extends State<ContentView> {
+  double _value = 50;
+  final double _MIN_VALUE = 1.0;
+  final double _MAX_VALUE = 100.0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("🎯🎯🎯🎯"),
+          Text("68"),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("${_MIN_VALUE.toInt()}"),
+                Expanded(child: Slider(value: _value, onChanged: _onChanged, min:_MIN_VALUE,max: _MAX_VALUE)),
+                Text("${_MAX_VALUE.toInt()}"),
+              ],
+            ),
+          ),
+          Text("$_value"),
+          ElevatedButton(onPressed: _onPressed, child: Text("TRY"))
+        ],
+      ),
+    );
+  }
+  
+  void _onChanged(double value) {
+    setState(() {
+      _value = value;
+    });
+    
+  }
+
+  void _onPressed() {
+    print("Button Pressed");
   }
 }
